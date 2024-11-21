617fe755559b423714e5670b333955fe0ead3bb0
taliamoszko <taliamoszko@gmail.com>
2024-11-21 19:28:05 +0200

    third version

--- Collatz.java
+++ Collatz.java
@@ -14,31 +14,31 @@ public class Collatz {
 
             
             if (seed == 1) {
-                sequence.append("1 4 2 1 ");  
+                sequence.append("1 4 2 1"); 
                 steps = 4;  
             } else {
-                
+              
                 while (current != 1) {
                     sequence.append(current).append(" ");
                     if (current % 2 == 0) {
                         current /= 2;  
                     } else {
-                        current = 3 * current + 1; 
+                        current = 3 * current + 1;  
                     }
                     steps++;  
                 }
                 sequence.append(1);  
                 steps++;  
-            }
+			}
 
             
             if (mode.equals("v")) {
-                
+               
                 System.out.println(sequence.toString() + " (" + steps + ")");
             }
         }
 
-       
+        
         System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
     }
 }
